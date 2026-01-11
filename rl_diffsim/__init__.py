704b484adcb5a478f0643d0544a53814744e9085
yufei4hua <michael4hua@gmail.com>
2026-01-11 20:09:59 +0100

    Change jax cache folder.

--- rl_diffsim/__init__.py
+++ rl_diffsim/__init__.py
@@ -1,5 +1,7 @@
 """LSY drone racing package for the Autonomous Drone Racing class @ TUM."""
 
+from pathlib import Path
+
 from crazyflow.utils import enable_cache
 
-enable_cache()  # Enable persistent caching of jax functions
+enable_cache(Path("./jax_cache"))  # Enable persistent caching of jax functions
